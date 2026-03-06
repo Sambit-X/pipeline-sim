e203d8f85ec271870ca1d9664c160fa9c4d36402
Sambit Kumar Mondal <sambitmondal02@gmail.com>
2026-03-06 11:02:02 +0530

    added google api and status

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const axios = require("axios");
 async function main() {
     const res = await axios.get("https://api.github.com");
     console.log("GitHub API status:", res.status);
+
+    const res2 = await axios.get("https://www.google.com");
+    console.log("Google API status:", res2.status);
 }
 
 main();
